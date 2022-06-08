6dc6fe46ae369e9821ffe93a87cea122d0b0b6bd
Saif Ur Rehman <86648367+saif-rehman-1@users.noreply.github.com>
2022-06-08 11:34:17 +0500

    UETBootcamp::Rails Day 3 (#5)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,24 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @products = User.first.products
+    @products = current_user.products
+  end
+
+  def new
+  end
+
+  def create
+    product = Product.new(product_params)
+    product.user_id = current_user.id
+
+    if product.save
+      redirect_to products_path
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:product_name, :quantity, :price)
   end
 end
